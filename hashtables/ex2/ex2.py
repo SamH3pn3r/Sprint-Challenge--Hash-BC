2ae5b7a02eec91e816217d93f92d33633b1ed27e
jurusteve <jstevemu17@gmail.com>
2019-12-06 13:39:47 -0500

    completed hashtable assignments. Still trying to mine a coin

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,16 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # Insert a source key and destination in the hashtable
+    for i in tickets:
+        hash_table_insert(hashtable, i.source, i.destination)
 
+    # find the first ticket with source as None. will become next destination
+    destination = hash_table_retrieve(hashtable, "NONE")
+
+    for i in range(length):
+        # build the route
+        route[i] = destination
+        # next destination
+        destination = hash_table_retrieve(hashtable, destination)
     return route
